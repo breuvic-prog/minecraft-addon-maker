242f68199a5d1c56dfb0ec0c814867070e332bf4
Broham223 <breuerv@uwplatt.edu>
2026-03-28 20:01:33 -0500

    Added Header component and started working on basics

---
+++ client/src/SiteHeader/SiteHeader.jsx
@@ -0,0 +1,15 @@
+import "./SiteHeader.css";
+
+const SiteHeader = () => {
+  return (
+    <header className="header">
+      <img src="/logo.png" alt="MC Addon Maker Logo" className="logo"/>
+      <nav>idk
+        </nav>
+    </header>
+  );
+};
+ 
+
+
+export default SiteHeader;
